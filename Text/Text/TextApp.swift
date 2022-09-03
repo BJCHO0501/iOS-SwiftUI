5909a56dd390aa7e90046827fce8dff5d6d71014
BJCHO0501 <bjcho1503@naver.com>
2022-09-03 23:37:34 +0900

    SwiftUI-Text 추가

---
+++ Text/Text/TextApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextApp.swift
+//  Text
+//
+//  Created by 조병진 on 2022/09/03.
+//
+
+import SwiftUI
+
+@main
+struct TextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
