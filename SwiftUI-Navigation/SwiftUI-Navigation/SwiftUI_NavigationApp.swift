d13aa6380ddd588edcae235c49b211e3889663b4
BJCHO0501 <bjcho1503@naver.com>
2022-09-16 23:55:54 +0900

    SwiftUi navigation

---
+++ SwiftUI-Navigation/SwiftUI-Navigation/SwiftUI_NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_NavigationApp.swift
+//  SwiftUI-Navigation
+//
+//  Created by 조병진 on 2022/09/10.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
